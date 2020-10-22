40092fdf77e98d11c52f37d559141c0a5bd6a4d4
Dirk Strauss <githubmine@outlook.com>
2020-10-22 08:58:53 +0200

    Added edit page

---
+++ VideoStore/VideoStore/Pages/Videos/Edit.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VideoStore.Core;
+using VideoStore.Data;
+
+namespace VideoStore.Pages.Videos
+{
+    public class EditModel : PageModel
+    {
+        private readonly IVideoData _videoData;
+
+        public Video Video { get; set; }
+
+        public EditModel(IVideoData videoData)
+        {
+            _videoData = videoData;
+        }
+
+        public IActionResult OnGet(int videoId)
+        {
+            Video = _videoData.GetVideo(videoId);
+
+            return Video == null ? RedirectToPage("./VideoError", new { message = "The video does not exist" }) : (IActionResult)Page();
+        }
+    }
+}
