4c92246384776fe7093f84f2330f324028b8ce89
Dirk Strauss <githubmine@outlook.com>
2020-10-23 15:34:57 +0200

    Modified Edit page

--- VideoStore/VideoStore/Pages/Videos/Edit.cshtml.cs
+++ VideoStore/VideoStore/Pages/Videos/Edit.cshtml.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Collections;
 using VideoStore.Core;
 using VideoStore.Data;
 
@@ -8,16 +11,20 @@ namespace VideoStore.Pages.Videos
     public class EditModel : PageModel
     {
         private readonly IVideoData _videoData;
+        private readonly IHtmlHelper _helper;
 
         public Video Video { get; set; }
+        public IEnumerable<SelectListItem> Genres { get; set; }
 
-        public EditModel(IVideoData videoData)
+        public EditModel(IVideoData videoData, IHtmlHelper helper)
         {
             _videoData = videoData;
+            _helper = helper;
         }
 
         public IActionResult OnGet(int videoId)
         {
+            Genres = _helper.GetEnumSelectList<MovieGenre>();
             Video = _videoData.GetVideo(videoId);
 
             return Video == null ? RedirectToPage("./VideoError", new { message = "The video does not exist" }) : (IActionResult)Page();
