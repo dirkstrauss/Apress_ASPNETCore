702c58c457f8e87d1c9834458d22f70039480d37
Dirk Strauss <githubmine@outlook.com>
2020-10-29 09:03:37 +0200

    Added a DbContext class to VideoStore.Data project.

---
+++ VideoStore/VideoStore.Data/VideoDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoStore.Core;
+
+namespace VideoStore.Data
+{
+    public class VideoDbContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+    }
+}
